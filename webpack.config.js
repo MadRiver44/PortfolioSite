74e9f6ace4ed5daaac34c1eed3d5306c9b1c3491
Kevin Turney <super996@optonline.net>
2017-12-18 10:02:02 -0500

    use name for hash

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ let config = {
   entry: path.resolve(__dirname, 'app/index.js'),
   output: {
     path: path.resolve(__dirname, '/public'),
-    filename: 'bundle.js',
+    filename: '[name].js',
   },
   module: {
     rules: [
