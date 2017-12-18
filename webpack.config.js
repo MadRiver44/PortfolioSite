6bf6550d969a36334843d88e746664bf63350e8f
Kevin Turney <super996@optonline.net>
2017-12-18 11:09:09 -0500

    revert back

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ let config = {
   entry: path.resolve(__dirname, 'app/index.js'),
   output: {
     path: path.resolve(__dirname, '/public'),
-    filename: '[name].js',
+    filename: 'bundle.js',
   },
   module: {
     rules: [
