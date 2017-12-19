224c4c712ce2b24dace906e3bfb22aada2224774
Kevin Turney <super996@optonline.net>
2017-12-19 14:08:25 -0500

    import styles for webpack

--- app/index.js
+++ app/index.js
@@ -2,5 +2,6 @@ import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
 import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom';
 import App from './Components/App';
+import '../public/styles.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
